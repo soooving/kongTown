c0a1720caf8b554146bc926a4a84cc92c42174dd
Subin Kim <soooving@gmail.com>
2023-11-20 11:43:04 +0900

    Initial commit : 11-20

---
+++ src/main/java/com/sb/kongtown/KongtownApplication.java
@@ -0,0 +1,13 @@
+package com.sb.kongtown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongtownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongtownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/kongtown/KongtownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.kongtown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongtownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
