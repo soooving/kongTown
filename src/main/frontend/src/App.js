69c33a01d4de1ee80898c324678f16764a0ee7e6
Subin Kim <soooving@gmail.com>
2023-11-23 12:08:49 +0900

    메뉴 설정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -15,9 +15,20 @@ function App() {
         <Sidebar />
 
         <Routes>
-          <Route path="/" exact element={<Main />} />
-          <Route path="/manage/product" element={<ProductManage />} />
-          <Route exact element={"4"} />
+            <Route path="/" exact element={<Main />} />
+            <Route path="/user/login" element={<ProductManage />} />
+
+            <Route path="/manage/firm" element={<ProductManage />} />
+            <Route path="/manage/firm/admin" element={<ProductManage />} />
+
+            <Route path="/manage/firm/user" element={<ProductManage />} />
+            <Route path="/manage/product" element={<ProductManage />} />
+            <Route path="/product/signup" element={<ProductManage />} />
+
+            <Route path="/product/rental" element={<ProductManage />} />
+            <Route path="/product/return" element={<ProductManage />} />
+
+            <Route exact element={<Main />} />
         </Routes>
       </>
   );
--- src/main/frontend/src/com/layout/header.js
+++ src/main/frontend/src/com/layout/header.js
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 const header = () => {
     return (
         <ul>
-            <li><Link to={"/"}>기업 관리</Link></li>
-            <li><Link to={"/"}>기업 관리자 관리</Link></li>
+            <li><Link to={"/user/login"}>로그인</Link></li>
+            <li><Link to={"/user/logout"}>로그아웃</Link></li>
 
-            <li><Link to={"/"}>사용자 관리</Link></li>
+            <li><Link to={"/manage/firm"}>기업 관리</Link></li>
+            <li><Link to={"/manage/firm/admin"}>기업 관리자 관리</Link></li>
+
+            <li><Link to={"/manage/firm/user"}>사용자 관리</Link></li>
             <li><Link to={"/manage/product"}>대여 물품 관리</Link></li>
-            <li><Link to={"/"}>물품 신청 관리</Link></li>
+            <li><Link to={"/product/signup"}>물품 신청 관리</Link></li>
 
-            <li><Link to={"/"}>물품 대여 신청</Link></li>
-            <li><Link to={"/"}>물품 반납 신청</Link></li>
+            <li><Link to={"/product/rental"}>물품 대여 신청</Link></li>
+            <li><Link to={"/product/return"}>물품 반납 신청</Link></li>
         </ul>
     );
 }
